ac5542ac63dba7acbc43b9f1c60e5b71e8e20f38
Wellington Lee <5994489+wellingtonlee@users.noreply.github.com>
2021-12-11 23:09:21 -0500

    Day 5 solution

---
+++ 5/day05-2.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Vent struct {
+	x1, x2, y1, y2 int
+	valid          bool
+}
+
+func run(s string) interface{} {
+	lines := strings.Split(s, "\n")
+
+	vents := make([]Vent, 1, len(lines)-1)
+	max_x, max_y := 0, 0
+
+	for _, raw := range lines[:len(lines)-1] {
+		coords := strings.Split(raw, " -> ")
+		a_vals := strings.Split(coords[0], ",")
+		b_vals := strings.Split(coords[1], ",")
+
+		x1, _ := strconv.Atoi(a_vals[0])
+		x2, _ := strconv.Atoi(b_vals[0])
+		y1, _ := strconv.Atoi(a_vals[1])
+		y2, _ := strconv.Atoi(b_vals[1])
+
+		//fmt.Printf("x: (%d, %d) | y: (%d, %d)\n", x1, x2, y1, y2)
+		vents = append(vents, Vent{x1, x2, y1, y2, true})
+		if x1 > max_x {
+			max_x = x1
+		}
+		if x2 > max_x {
+			max_x = x2
+		}
+		if y1 > max_y {
+			max_y = y1
+		}
+		if y2 > max_y {
+			max_y = y2
+		}
+
+	}
+	max_x += 1
+	max_y += 1
+
+	var floor [][]int
+	for i := 0; i < max_x; i++ {
+		row := make([]int, max_y)
+		floor = append(floor, row)
+	}
+	start, end := 0, 0
+
+	for _, vent := range vents {
+		//fmt.Printf("x: (%d, %d) | y: (%d, %d) | %t\n", vent.x1, vent.x2, vent.y1, vent.y2, vent.valid)
+		if vent.valid {
+			if vent.x1 == vent.x2 {
+				start = int(math.Min(float64(vent.y1), float64(vent.y2)))
+				end = int(math.Max(float64(vent.y1), float64(vent.y2)))
+
+				for j := start; j <= end; j++ {
+					floor[vent.x1][j] += 1
+				}
+			} else if vent.y1 == vent.y2 {
+				start = int(math.Min(float64(vent.x1), float64(vent.x2)))
+				end = int(math.Max(float64(vent.x1), float64(vent.x2)))
+
+				for j := start; j <= end; j++ {
+					floor[j][vent.y1] += 1
+				}
+			} else {
+				slope := 0
+				var start_x int
+				var end_x int
+				var start_y int
+				if vent.x1 < vent.x2 {
+					start_x = vent.x1
+					end_x = vent.x2
+					start_y = vent.y1
+					if vent.y1 < vent.y2 {
+						slope = 1
+					} else {
+						slope = -1
+					}
+				} else {
+					start_x = vent.x2
+					end_x = vent.x1
+					start_y = vent.y2
+					if vent.y2 < vent.y1 {
+						slope = 1
+					} else {
+						slope = -1
+					}
+				}
+
+				j := start_y
+				for i := start_x; i <= end_x; i++ {
+					floor[i][j] += 1
+					j += slope
+				}
+			}
+		}
+	}
+
+	num_overlap := 0
+
+	for i := 0; i < max_x; i++ {
+		for j := 0; j < max_y; j++ {
+			if floor[i][j] > 1 {
+				num_overlap += 1
+			}
+		}
+	}
+
+	return num_overlap
+}
+
+func main() {
+
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	start := time.Now()
+	result := run(string(input))
+
+	fmt.Printf("Duration: %f\n", time.Now().Sub(start).Seconds()*1000)
+	fmt.Print("Result: ")
+	fmt.Println(result)
+}
